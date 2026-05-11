371d88e17e795dcefd68234f45c28a9c4f8ce76d
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2026-05-11 12:49:51 -0400

    rental bug2

--- item-service/src/main/java/com/smartiadev/item_service/kafka/RentalApprovedConsumer.java
+++ item-service/src/main/java/com/smartiadev/item_service/kafka/RentalApprovedConsumer.java
@@ -1,12 +1,10 @@
 package com.smartiadev.item_service.kafka;
 
-
-
 import com.smartiadev.base_domain_service.dto.RentalApprovedEvent;
+import com.smartiadev.base_domain_service.dto.RentalStartedEvent;
 import com.smartiadev.item_service.entity.Item;
 import com.smartiadev.item_service.entity.ItemStatus;
 import com.smartiadev.item_service.repository.ItemRepository;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
@@ -17,16 +15,16 @@ public class RentalApprovedConsumer {
 
     private final ItemRepository itemRepository;
 
+    // 🔒 Item verrouillé dès approbation
     @KafkaListener(
             topics = "rental.approved",
             groupId = "item-service-group"
     )
-    public void consume(RentalApprovedEvent event) {
+    public void consumeRentalApproved(RentalApprovedEvent event) {
 
         Item item = itemRepository.findById(event.itemId())
                 .orElseThrow(() -> new RuntimeException("Item not found"));
 
-        // 🔒 verrouillage
         item.setActive(false);
         item.setStatus(ItemStatus.INACTIVE);
 
@@ -34,5 +32,22 @@ public class RentalApprovedConsumer {
 
         System.out.println("🔒 Item locked after rental approval: " + event.itemId());
     }
-}
 
+    // ▶️ Sécurité supplémentaire quand location démarre immédiatement
+    @KafkaListener(
+            topics = "rental.started",
+            groupId = "item-service-group"
+    )
+    public void consumeRentalStarted(RentalStartedEvent event) {
+
+        Item item = itemRepository.findById(event.itemId())
+                .orElseThrow(() -> new RuntimeException("Item not found"));
+
+        item.setActive(false);
+        item.setStatus(ItemStatus.INACTIVE);
+
+        itemRepository.save(item);
+
+        System.out.println("▶️ Item locked after rental started: " + event.itemId());
+    }
+}
