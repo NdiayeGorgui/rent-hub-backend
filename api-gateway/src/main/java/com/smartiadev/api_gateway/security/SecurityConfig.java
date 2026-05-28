f5522225a0b06e3f05af27d461852339943c4c72
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2026-05-28 15:48:48 -0400

    fqs

--- api-gateway/src/main/java/com/smartiadev/api_gateway/security/SecurityConfig.java
+++ api-gateway/src/main/java/com/smartiadev/api_gateway/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
@@ -78,6 +79,16 @@ public class SecurityConfig {
                                 "/api/users/internal/*/auction-strike",
                                 "/api/profile/**",
                                 "/api/stats/**"
+                        ).permitAll()
+                        .pathMatchers(
+                                HttpMethod.GET,
+                                "/api/faqs",
+                                "/api/faqs/theme/**"
+                        ).permitAll()
+
+                        .pathMatchers(
+                                HttpMethod.PUT,
+                                "/api/faqs/*/feedback"
                         ).permitAll()
                         .pathMatchers(
                                 "/ws-notifications/**",
