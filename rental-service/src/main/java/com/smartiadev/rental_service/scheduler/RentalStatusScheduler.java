90a58d6b6480089b083560d10f8707e7da1639a5
NdiayeGorgui <90470613+NdiayeGorgui@users.noreply.github.com>
2026-06-02 13:06:05 -0400

    rentals6

--- rental-service/src/main/java/com/smartiadev/rental_service/scheduler/RentalStatusScheduler.java
+++ rental-service/src/main/java/com/smartiadev/rental_service/scheduler/RentalStatusScheduler.java
@@ -2,6 +2,7 @@ package com.smartiadev.rental_service.scheduler;
 
 import com.smartiadev.base_domain_service.dto.RentalStartedEvent;
 import com.smartiadev.rental_service.client.ItemClient;
+import com.smartiadev.rental_service.dto.ItemInternalDTO;
 import com.smartiadev.rental_service.entity.RentalStatus;
 import com.smartiadev.rental_service.entity.Rental;
 import com.smartiadev.rental_service.kafka.RentalEventProducer;
@@ -13,6 +14,8 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
@@ -35,12 +38,23 @@ public class RentalStatusScheduler {
                         RentalStatus.APPROVED,
                         today
                 );
+        // 📦 1. batch items (ICI)
+        List<Long> itemIds = toStart.stream()
+                .map(Rental::getItemId)
+                .toList();
 
+        Map<Long, ItemInternalDTO> items = itemClient.getItemsBatch(itemIds)
+                .stream()
+                .collect(Collectors.toMap(ItemInternalDTO::id, i -> i));
+
+        // 🔁 2. boucle rentals
         for (Rental rental : toStart) {
+
             rental.setStatus(RentalStatus.ONGOING);
-            // 📦 récupérer item
-            var item = itemClient.getItem(rental.getItemId());
-            String itemTitle = item.title();
+
+            ItemInternalDTO item = items.get(rental.getItemId());
+            String itemTitle = (item != null) ? item.title() : "Item";
+
             eventProducer.sendRentalStarted(
                     new RentalStartedEvent(
                             rental.getId(),
